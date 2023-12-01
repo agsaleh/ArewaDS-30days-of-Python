63c0e0cde359dc2a8a06d65556136641eabbeca8
Abubakar G. Saleh <141282589+agsaleh@users.noreply.github.com>
2023-12-01 01:25:33 +0100

    my first commit!
    Submitted day one to day three challenge.

---
+++ Day-03.py
@@ -0,0 +1,16 @@
+# Area of Triangle
+age = int
+height = float(input('Height:'))
+base = float(input('Base:'))
+area = 0.5 * base * height
+print('Area of Triangle is:', area)
+
+
+# perimeter of the triangle
+
+side_a = float(input('Side A:'))
+side_b = float(input('Side B:'))
+side_c = float(input('Side C:'))
+perimeter = side_a + side_b + side_c
+
+print('Perimeter of Triangle is:', perimeter)
---
+++ sqrt_calculator.py
@@ -0,0 +1,4 @@
+import math
+number = float(input('Enter a Number: '))
+sq_root = math.sqrt(number)
+print(f'The square root of {number} is {sq_root}')
