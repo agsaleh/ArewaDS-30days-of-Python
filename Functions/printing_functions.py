3f22b25b223a348c2ff60e3be5b42c40c5e27410
Abubakar G. Saleh <141282589+agsaleh@users.noreply.github.com>
2024-01-08 12:48:12 +0100

    Function exercises

---
+++ Functions/printing_functions.py
@@ -0,0 +1,10 @@
+def print_models(models):
+    """Simulate printing each model."""
+    for model in models:
+        print(f'Printing {model}')
+
+def show_completed_models(completed_models):
+    """Show all the completed models."""
+    print('\nThe following models have been printed:')
+    for model in completed_models:
+        print(model)
---
+++ Functions/printing_models.py
@@ -0,0 +1,7 @@
+from printing_functions import print_models, show_completed_models
+
+models_to_print = ['phone', 'tablet', 'laptop']
+completed_models = []
+
+print_models(models_to_print)
+show_completed_models(models_to_print)
